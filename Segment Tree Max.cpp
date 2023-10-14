a7cb78af29e02348bc25ae2d98b24f8833cc8859
Nobel Ahmad Badhon <107022184+BadhonAhmad@users.noreply.github.com>
2023-10-14 15:09:53 +0600

    Update Segment Tree Max.cpp

--- Segment Tree Max.cpp
+++ Segment Tree Max.cpp
@@ -1,41 +1,63 @@
+/*
+This is a code segment tree with build,query,update function to find the max element.
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
-int a[100005], seg[4 * 100005];
+int a[100005], tree[4 * 100005];
 
-void build(int ind, int low, int high){
+void build(int node, int low, int high){
 
     //if returns bactracking will be execute 
     if(low == high){
-        seg[ind] = a[low];
+        tree[node] = a[low];
         return;
     }
 
     //if doesn't match the base case execution starts from here
     int mid=(low + high) / 2;
-    build(2 * ind, low, mid);//(2 * 1 , 1 , 4) [for 1 to 8]
-    build(2 * ind + 1, mid + 1, high);//(2 * 1 + 1, 5 , 8) [for 3 to 8]
+    build(2 * node, low, mid);//(2 * 1 , 1 , 4) [for 1 to 8]
+    build(2 * node + 1, mid + 1, high);//(2 * 1 + 1, 5 , 8) [for 3 to 8]
 
     //backtracking
-    seg[ind] = max(seg[2 * ind],seg[2 * ind + 1]); 
+    tree[node] = max(tree[2 * node],tree[2 * node + 1]); 
 
 }
 
-int query(int ind, int low, int high, int l, int r){//l,r is to compare whether the range lies or not between low and high[index of nodes]
+int query(int node, int low, int high, int l, int r){//l,r is to compare whether the range lies or not between low and high[index of nodes]
 
     //base case -> completely lies
     if(low >= l && high <= r){
-        return seg[ind];
+        return tree[node];
     }
 
     if(high < l || low > r) return INT_MIN;//high is less than l that means out of range
 
     //moving towards the left and right subtree finding the max of them
     int mid = (low + high) / 2;
-    int left = query(2 * ind, low , mid, l, r);
-    int right =query(2 * ind + 1, mid + 1, high, l, r);
+    int left = query(2 * node, low , mid, l, r);
+    int right =query(2 * node + 1, mid + 1, high, l, r);
     return max(left , right);
 
 }
+
+void update(int node, int low, int high, int idx, int val){
+    if(low == high){
+        tree[node] = val;
+        a[low] = val;
+        return;
+    }
+
+    int mid = (low + high) / 2;
+    if(idx <= mid){
+        update(2 * node, low, mid, idx, val);
+    }
+    else{
+        update(2 * node + 1, mid + 1, high, idx, val);
+    }
+
+    tree[node] = max(tree[2 * node],tree[2 * node + 1]); 
+} 
 
 int main()
 {
@@ -51,9 +73,27 @@ int main()
     cin >> q;
     while (q--)
     {
-        int l, r;
-        cin >> l >> r;
+        int type,l, r;
+        cin >> type >> l >> r;
+        if(type  == 1)
         cout << query(1, 1, n, l, r) << endl;
+        else{
+            update(1, 1, n, l, r);
+        }
     }
     
 }
+
+/*
+test case:
+4
+1 2 3 4
+4
+2 3 10
+1 1 4
+2 1 1111
+1 1 3
+output:
+10
+1111
+*/
