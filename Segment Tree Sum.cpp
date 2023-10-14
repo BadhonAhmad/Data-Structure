6a6af7dfbb0f5df0baab6dff0bcfd71db15d3ceb
Nobel Ahmad Badhon <107022184+BadhonAhmad@users.noreply.github.com>
2023-10-14 15:26:10 +0600

    Update Segment Tree Sum.cpp

--- Segment Tree Sum.cpp
+++ Segment Tree Sum.cpp
@@ -1,41 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a[100005], seg[4 * 100005];
+int a[100005], tree[4 * 100005];
 
-void build(int ind, int low, int high){
+void build(int node, int low, int high){
 
     //if returns bactracking will be execute 
     if(low == high){
-        seg[ind] = a[low];
+        tree[node] = a[low];
         return;
     }
 
     //if doesn't match the base case execution starts from here
     int mid=(low + high) / 2;
-    build(2 * ind, low, mid);//(2 * 1, 1 , 4) [for 1 to 8]
-    build(2 * ind + 1, mid + 1, high);//(2 * 1 + 1, 5 , 8)
+    build(2 * node, low, mid);//(2 * 1 , 1 , 4) [for 1 to 8]
+    build(2 * node + 1, mid + 1, high);//(2 * 1 + 1, 5 , 8) [for 3 to 8]
 
     //backtracking
-    seg[ind] = seg[2 * ind] + seg[2 * ind + 1]; 
+    tree[node] = tree[2 * node] + tree[2 * node + 1]; 
 
 }
 
-int query(int ind, int low, int high, int l, int r){//l,r is to compare whether the range lies or not between low and high[index of nodes]
+int query(int node, int low, int high, int l, int r){//l,r is to compare whether the range lies or not between low and high[index of nodes]
 
     //base case -> completely lies
     if(low >= l && high <= r){
-        return seg[ind];
+        return tree[node];
     }
 
     if(high < l || low > r) return 0;//high is less than l that means out of range
 
-    //moving towards the left and right subtree finding the max of them
+    //moving towards the left and right subtree finding the sum of them
     int mid = (low + high) / 2;
-    int left = query(2 * ind, low , mid, l, r);
-    int right =query(2 * ind + 1, mid + 1, high, l, r);
+    int left = query(2 * node, low , mid, l, r);
+    int right =query(2 * node + 1, mid + 1, high, l, r);
     return left + right;
 
 }
+
+void update(int node, int low, int high, int idx, int val){
+    if(low == high){
+        tree[node] = val;
+        a[low] = val;
+        return;
+    }
+
+    int mid = (low + high) / 2;
+    if(idx <= mid){
+        update(2 * node, low, mid, idx, val);
+    }
+    else{
+        update(2 * node + 1, mid + 1, high, idx, val);
+    }
+
+    tree[node] = (tree[2 * node] + tree[2 * node + 1]); 
+} 
 
 int main()
 {
@@ -45,15 +63,33 @@ int main()
         cin >> a[i];
     }
 
-    build(1, 1, n);// (index of root, start index, ending index of range)
+    build(1, 1, n);// (index of root, start index, ending index of range)//one based indexing
     
     int q;
     cin >> q;
     while (q--)
     {
-        int l, r;
-        cin >> l >> r;
+        int type,l, r;
+        cin >> type >> l >> r;
+        if(type  == 1)
         cout << query(1, 1, n, l, r) << endl;
+        else{
+            update(1, 1, n, l, r);
+        }
     }
     
 }
+
+/*
+test case:
+4
+1 1 1 1
+4
+2 3 10
+1 1 4
+2 1 10
+1 1 3
+output:
+13
+21
+*/
